2c601528c3bc6f2640fc8126bc742e0155434f71
FarzanaChowdhury <sumaiya.farzana.eleen@gmail.com>
2024-07-11 23:36:29 +0600

    edit labels

--- src/Form.js
+++ src/Form.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import { jsPDF } from 'jspdf';
 import './CSS/styles.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPen } from '@fortawesome/free-solid-svg-icons';
-import { handleChange, handleLabelChange, toggleEdit, calculateTotal } from './Functions.js'
+import { faPen, faSave, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { handleChange, handleLabelChange, toggleEdit } from './Functions.js'
 
 function BillCalculator() {
   const [bills, setBills] = useState([
@@ -13,34 +13,31 @@ function BillCalculator() {
     { bill1: 0, bill2: 0, bill3: 0, total: 0 },
   ]);
 
-  const [labels, setLabel] = useState(
-
-    [{ label1: 'Water Bill', label2: 'Gas Bill', label3: 'Electricity Bill' },
+  const [labels, setLabel] = useState([
     { label1: 'Water Bill', label2: 'Gas Bill', label3: 'Electricity Bill' },
     { label1: 'Water Bill', label2: 'Gas Bill', label3: 'Electricity Bill' },
     { label1: 'Water Bill', label2: 'Gas Bill', label3: 'Electricity Bill' },
-    ]
-  )
+    { label1: 'Water Bill', label2: 'Gas Bill', label3: 'Electricity Bill' },
+  ]);
 
   const [isEditingBill, setEditBill] = useState(
-          Array(bills.length).fill({ bill: false, bill2: false, bill3: false })
-    
+    Array(bills.length).fill({ bill1: false, bill2: false, bill3: false })
   );
-
 
   const [isEditingLabel, setEditLabel] = useState(
     Array(labels.length).fill({ label1: false, label2: false, label3: false })
   );
 
+  const [backupLabels, setBackupLabels] = useState(labels);
+
+
   const exportPDF = () => {
     const doc = new jsPDF();
 
     bills.forEach((bill, i) => {
-      // Determine the column and row positions
-      const x = (i % 2) * 100 + 20; // 100 points between columns, 10 points margin
-      const y = Math.floor(i / 2) * 60 + 10; // 60 points between rows, 10 points margin
+      const x = (i % 2) * 100 + 20;
+      const y = Math.floor(i / 2) * 60 + 10;
 
-      // Adjust the y-coordinates for each text line within the form
       doc.text(`Floor ${i + 1}`, x, y);
       doc.text(`${labels[i].label1}: ${bill.bill1}`, x, y + 10);
       doc.text(`${labels[i].label2}: ${bill.bill2}`, x, y + 20);
@@ -51,87 +48,61 @@ function BillCalculator() {
     doc.save('bill-summary.pdf');
   };
 
+  const startEditing = (index, labelField) => {
+    setBackupLabels([...labels]);
+    toggleEdit(index, labelField, isEditingLabel, setEditLabel);
+  };
+
+  const cancelEditing = (index, labelField) => {
+    setLabel(backupLabels);
+    toggleEdit(index, labelField, isEditingLabel, setEditLabel);
+  };
+
+  const renderInput = (index, billField, labelField) => (
+    <>
+      <button className='edit' type="button">
+        <FontAwesomeIcon icon={isEditingLabel[index][labelField] ? faSave : faPen} onClick={() => startEditing(index, labelField)} />
+      </button>
+      {isEditingLabel[index][labelField] ? (
+        <><input
+          type='text'
+          value={labels[index][labelField]}
+          onChange={(e) => handleLabelChange(index, labelField, e.target.value, labels, setLabel)} />
+
+          <button className='cancel' type='button' onClick={() => cancelEditing(index, labelField)}><FontAwesomeIcon icon={faTimes} /></button>
+
+        </>
+      ) : (
+        <label>
+          {labels[index][labelField]}:
+          <input
+            type="number"
+            value={bills[index][billField]}
+            onChange={(e) => handleChange(index, billField, e.target.value, bills, setBills)}
+          />
+        </label>
+      )}
+      <br /><br />
+    </>
+  );
 
   return (
     <div>
       <h2>Bill Calculator</h2>
       <div className='grid-container'>
-
         {bills.map((bill, index) => (
           <div key={index}>
             <h3>Floor {index + 1}</h3>
-
             <form>
-            <button className='edit'>
-                <FontAwesomeIcon icon={faPen} 
-                onClick={() => toggleEdit(index, 'label1' , isEditingLabel, setEditLabel)}/>
+              {renderInput(index, 'bill1', 'label1')}
+              {renderInput(index, 'bill2', 'label2')}
+              {renderInput(index, 'bill3', 'label3')}
 
-              </button>
 
-              {isEditingLabel[index].label1 ?
-                <input
-                  type='string'
-                  value={labels[index].label1}
-                  onChange={(e) => handleLabelChange(index, 'label1', e.target.value, labels, setLabel)}
-              
-                /> :
-                <label>
-                  {labels[index].label1}:
-                  <input
-                    type="number"
-                    value={bill.bill1}
-                    onChange={(e) => handleChange(index, 'bill1', e.target.value, bills, setBills)}
-                  />
-                </label>}
-              <br /><br />
-              <button className='edit'>
-                <FontAwesomeIcon icon={faPen} 
-                onClick={() => toggleEdit(index, 'label1' , isEditingLabel, setEditLabel)}/>
-
-              </button>
-
-              {isEditingLabel[index].label2 ?
-                <input
-                  type='string'
-                  value={labels[index].label2}
-                  onChange={(e) => handleLabelChange(index, 'label2', e.target.value, labels, setLabel)}
-              
-                /> :
-                <label>
-                  {labels[index].label3}:
-                  <input
-                    type="number"
-                    value={bill.bill3}
-                    onChange={(e) => handleChange(index, 'bill2', e.target.value, bills, setBills)}
-                  />
-                </label>}
-              <br /><br />
-
-              <button className='edit'>
-                <FontAwesomeIcon icon={faPen} 
-                onClick={() => toggleEdit(index, 'label2' , isEditingLabel, setEditLabel)}/>
-
-              </button>
-
-              {isEditingLabel[index].label3 ?
-                <input
-                  type='string'
-                  value={labels[index].label3}
-                  onChange={(e) => handleLabelChange(index, 'label3', e.target.value, labels, setLabel)}
-              
-                /> :
-                <label>
-                  {labels[index].label3}:
-                  <input
-                    type="number"
-                    value={bill.bill3}
-                    onChange={(e) => handleChange(index, 'bill3', e.target.value, bills, setBills)}
-                  />
-                </label>}
-              <br /><br />
 
               {/* <button type="button" onClick={() => calculateTotal(index, bills, setBills)}>Calculate Total</button> */}
             </form>
+
             <p>Total: {bill.total}</p>
           </div>
         ))}
--- src/Functions.js
+++ src/Functions.js
@@ -1,31 +1,37 @@
 import React, { useState } from 'react';
 
 
-export const handleChange = (index, field, value, bills, setBills, isEditingBill, setEditBill) => {
-    const updatedBills = bills.map((bill, i) =>
-      i === index ? { ...bill, [field]: parseFloat(value) || 0 } : bill
-    );
-    setBills(updatedBills);
-    setEditBill(isEditingBill? false : true)
-  };
+export const handleChange = (index, field, value, bills, setBills) => {
+  const updatedBills = bills.map((bill, i) =>
+    i === index ? { ...bill, [field]: parseFloat(value) || 0 } : bill
+  );
+  setBills(updatedBills);
+  calculateTotal(index, updatedBills, setBills);
+};
 
- export const handleLabelChange = (index) => {
+export const handleLabelChange = (index, field, value, labels, setLabel) => {
+  const updateLabels = labels.map((label, i) =>
+    i === index ? { ...label, [field]: value } : label);
 
-  }
+  setLabel(updateLabels);
+}
 
-  export const toggleEdit = (index, label, isEditingLabel, setEditLabel) => {
-    
-setEditLabel(isEditingLabel ? false : true)
-  }
+export const toggleEdit = (index, field, isEditingLabel, setEditLabel) => {
+  const updateEdit = isEditingLabel.map((edit,i)=>
+    i=== index? {...edit, [field] : !edit[field]} : edit
+  );
 
-  export const calculateTotal = (index, bills, setBills) => {
-    const updatedBills = bills.map((bill, i) => {
-      if (i === index) {
-        const totalAmount = bill.bill1 + bill.bill2 + bill.bill3;
-        return { ...bill, total: totalAmount };
-      }
-      return bill;
-    });
-    setBills(updatedBills);
-  };
+  setEditLabel(updateEdit)
+}
+
+export const calculateTotal = (index, bills, setBills) => {
+  const updatedBills = bills.map((bill, i) => {
+    if (i === index) {
+      const totalAmount = bill.bill1 + bill.bill2 + bill.bill3;
+      return { ...bill, total: totalAmount };
+    }
+    return bill;
+  });
+  setBills(updatedBills);
+};
 
